d997a999d23867082711f2103a246bbb4f61ab24
Evelin Reyes <evelin.reyes@educa.madrid.org>
2021-10-28 16:15:20 +0200

    En

--- PrimeraTarea.java
+++ PrimeraTarea.java
@@ -2,7 +2,7 @@ public class PrimeraTarea {
     public static void main(String[] args) {
         System.out.println("Hola, mundo");
         System.out.println("Módulo: Entornos de Desarrollo");
-		System.out.println("Esto lo añado para ver si GIT   recoge los cambios");
+		System.out.println("Esto lo añado para ver si GIT recoge los cambios");
 		System.out.println("Una nuevo cambio..");
     }
 }
